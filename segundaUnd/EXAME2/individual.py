489e120c29c7836f859ef2bc71bbecdb126d95d1
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-10-20 17:32:17 -0300

    Implementação Exame 2 + .gitignore

---
+++ segundaUnd/EXAME2/individual.py
@@ -0,0 +1,80 @@
+import random
+from math import sin, pi, sqrt, exp
+
+MIN = -10
+MAX = 10
+MUTATION_RATE = 0.01
+
+class Individual:
+
+    def __init__(self):
+        self.chromosome = [
+            random.uniform(MIN, MAX),
+            random.uniform(MIN, MAX)
+        ]
+
+    def __repr__(self):
+        return f"({self.chromosome[0]:3}, {self.chromosome[1]:3})"
+
+    def fitness(self):
+        x = self.chromosome[0]
+        y = self.chromosome[1]
+        
+        t = sqrt((x**2 / pi) + (y**2 / pi))
+        try: 
+            return sin(t) / t
+        except:
+            return 1
+        #return 0.5*x*exp(-(x**2)/15.0-(y**2)/15.0)
+
+    def crossover(self, partner, crossoverType):
+        child = Individual()
+
+        # Clonar o de maior fitness
+        if (crossoverType == "ELITE"):
+            if (self.fitness() < partner.fitness()):
+                child.chromosome = self.chromosome
+            else:
+                child.chromosome = partner.chromosome
+        # Herança aleatória
+        elif (crossoverType == "HALF"):
+            if (random.random() < 0.5):
+                child.chromosome[0] = self.chromosome[0]
+            else:
+                child.chromosome[0] = partner.chromosome[0]
+            
+            if (random.random() < 0.5):
+                child.chromosome[1] = self.chromosome[1]
+            else:
+                child.chromosome[1] = partner.chromosome[1]
+        # Garantir que o de melhor fitness ao menos um cromossomo
+        elif (crossoverType == "HALF_ELITE"):
+            cont = 0
+            if (self.fitness() < partner.fitness()):
+                if(random.random() < 0.5):
+                    child.chromosome[0] = self.chromosome[0]
+                    cont += 1
+                else:
+                    child.chromosome[1] = self.chromosome[1]
+            else:
+                if(random.random() < 0.5):
+                    child.chromosome[0] = partner.chromosome[0]
+                    cont += 1
+                else:
+                    child.chromosome[1] = partner.chromosome[1]
+            
+            if (random.random() < 0.5):
+                child.chromosome[cont] = self.chromosome[cont]
+            else:
+                child.chromosome[cont] = partner.chromosome[cont]
+
+        return child
+
+    def mutate(self):
+        if (random.random() < MUTATION_RATE):     
+            self.chromosome = [
+                random.uniform(MIN, MAX),
+                random.uniform(MIN, MAX)
+            ]
+        
+
---
+++ segundaUnd/EXAME2/main.py
@@ -0,0 +1,67 @@
+from individual import Individual
+from random import choice
+from math import floor
+from sys import argv
+
+
+def mapRange(currentRange, desiredRange, value):
+    (a1, a2), (b1, b2) = currentRange, desiredRange
+    return b1 + ((value - a1) * (b2 - b1) / (a2 - a1))
+
+
+if __name__ == '__main__':
+    try:
+        crossoverType = argv[1]
+        maxGeneration = int(argv[2])
+        popSize = int(argv[3])
+    except:
+        print(f"Arguments missing!\nUsage: {argv[0]} crossoverType maxGeneration popSize")
+        print("crossoverType: ELITE, HALF, HALF_ELITE")
+        exit()
+
+
+    population = [Individual() for _ in range(popSize)]
+
+    generation = 0
+    previousBest = population[0]
+    while (generation < maxGeneration):
+
+        # Encontarar o mehlor individuo
+        minFitness = 1
+        best = None
+        for individual in population:
+            if (individual.fitness() < minFitness):
+                minFitness = individual.fitness()
+                best = individual
+
+        # Gerar a matting Pool
+        mattingPool = []
+        for individual in population:
+            fitnessNormalized = mapRange(
+                (-1, 1), (1, 0),
+                individual.fitness()
+            )
+
+            for _ in range(floor(fitnessNormalized * 10)):
+                mattingPool.append(individual)
+
+        # Gera a nova população
+        population = []
+        for _ in range(popSize):
+            # Seleciona os pais
+            partner_1 = mattingPool[choice(range(0, len(mattingPool)))]
+            partner_2 = mattingPool[choice(range(0, len(mattingPool)))]
+
+            # Gera a prole
+            child = partner_1.crossover(partner_2, crossoverType)
+            child.mutate()
+
+            population.append(child)
+
+        if (previousBest.fitness() != best.fitness()):
+            print(f'Generation: {generation:3}')
+            print(f'\tBest: {best} Fitness: {best.fitness()}')
+
+        previousBest = best
+
+        generation += 1
